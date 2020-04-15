eb06f01e4b3e4da886f70e5b572270845e9de84f
Sherry_Jiang <sherry@Sherry-Jiangs-MacBook-Pro.local>
2020-04-15 17:27:42 +0800

    Add Reason 2 chart (gener reason)

--- src/components/Reasons/Reason2.vue
+++ src/components/Reasons/Reason2.vue
@@ -1,17 +1,81 @@
 <template>
-  <v-container>
-      <v-row
-          align="center"
-          justify="center"
-        >
-      <h1> Reason 2 Placeholder </h1>
-      </v-row>
-  </v-container>
+<div>
+    <b-card></b-card>
+    <b-card-group deck>
+      <b-card>
+       <apexchart type="scatter" height="350" :options="chartOptions" :series="series"></apexchart>
+      </b-card> 
+      <b-card
+       title="Definition"
+       class="text-left"
+      >
+      <b-card-text>
+      Adolescent birth rate: number of live births to adolescent women per 1,000 adolescent women
+      </b-card-text>
+      <b-card-text>
+      Gender parity index: female / male enrollment rate
+      </b-card-text>
+      </b-card>
+    </b-card-group>
+  </div>
+
 </template>
 
 <script>
+import VueApexCharts from 'vue-apexcharts';
+import {pair_post,pair_lowersecond,pair_uppersecond, pair_primary, pair_bachelor} from '../../assets/reason2';
 export default {
-
+  components: { 
+    apexchart: VueApexCharts
+  },
+  data() {
+    return {
+      series: [
+        {
+            name: "Post Secondary",
+            data: pair_post,
+          },{
+            name: "Lower Secondary",
+            data: pair_lowersecond
+          },
+          {
+            name: "Upper Secondary",
+            data: pair_uppersecond
+          },
+          {
+            name: "Primary",
+            data: pair_primary
+          },
+          {
+            name: "Bachelor",
+            data: pair_bachelor,
+          }
+          ],
+          chartOptions: {
+            chart: {
+              height: 350,
+              type: 'scatter',
+              zoom: {
+                enabled: true,
+                type: 'xy'
+              }
+            },
+            xaxis: {
+              title: "Adolescent Birth Rate ",
+              tickAmount: 10,
+              labels: {
+                formatter: function(val) {
+                  return parseFloat(val).toFixed(1)
+                }
+              }
+            },
+            yaxis: {
+              title: "Gender Parity Index",
+              tickAmount: 7
+            }
+          },
+    }
+  }
 }
 </script>
 
