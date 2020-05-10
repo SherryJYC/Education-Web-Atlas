fc5dd297f49fef15f99c8eea46d202a5acf51705
Sherry_Jiang <sherry@student-net-vpn-0493.ethz.ch>
2020-05-10 10:52:55 +0800

    adjust color

--- src/components/Reasons/Reason1.vue
+++ src/components/Reasons/Reason1.vue
@@ -79,7 +79,7 @@ export default {
       titleSetting: {
         text: 'Gender Parity Index in Primary Level'
       },
-      colorScale: ['rgb(153,51,51)','rgb(204,0,0)','rgb(204,153,48)','rgb(255,204,102)'],//["#FFC300","#FF5733","#C70039","#900C3F"],
+      colorScale: ['rgb(255,204,102)','rgb(204,153,48)','rgb(204,0,0)','rgb(153,51,51)',],//["#FFC300","#FF5733","#C70039","#900C3F"],
       value: {
         key: "invest",
         metric: "% GDP"
